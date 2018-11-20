cc90db68e4a57fd81cdf6a8ace78c73a7db3aa04
Cameron Grams <cameron.grams.2@gmail.com>
2018-11-19 22:17:59 -0500

    add CORS permissions

--- testServer.js
+++ testServer.js
@@ -3,6 +3,13 @@ const port = process.env.PORT || 8888;
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+});
+
 app.get('/', function (req, res) {
   return res.json( { "message": "good on server" } );
 });
